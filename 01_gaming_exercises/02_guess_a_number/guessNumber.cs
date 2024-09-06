a8cebf2161defe186c5602bea7a8ec6902a3f93b
wavynate1k <nathanspaul12345@icloud.com>
2024-09-06 14:43:08 -0400

    guessNumber

--- 01_gaming_exercises/02_guess_a_number/guessNumber.cs
+++ 01_gaming_exercises/02_guess_a_number/guessNumber.cs
@@ -3,6 +3,7 @@ class GuessNumber {
     static void Main() {
       int numGuess = 0;
       int maxGuess = 7;
+      int guess; 
 
       // Generate the secret number here.
       Random rnd = new Random(); // Create an object named 'rnd' that isa copy of the Random() class.
@@ -10,12 +11,34 @@ class GuessNumber {
       int secretNumber = rnd.Next(25, 1000); // Generate from ? to ?
       Console.WriteLine(secretNumber);
       
-      // while loop
-        // Allow the user to guess the number.
-        // Compare the guess to the secret number.
-        // If match, print win, if > or < give a hint.
-        // +1 to numGuess
-        // Check if numGuess > maxGuesse
-        // If true, print a "lose game" message, if false guess again.
+      while (numGuess < maxGuess)
+      {
+          Console.WriteLine("Secret Number: " + secretNumber);
+          Console.WriteLine("Please guess an integer between 25 and 1000.\n");       
+          guess = Convert.ToInt32(Console.ReadLine());
+          Console.WriteLine("Guess" + guess);
+
+          numGuess++;
+          if (guess < secretNumber)
+          {
+              Console.WriteLine("Your guess is too low.\n");
+          }
+          else if (guess > secretNumber)
+          {
+            Console.WriteLine("Your guess is too high.\n")
+          } 
+          else
+          {
+            Console.WriteLine("You guessed correctley.\n")
+            break;
+          }
+
+          if (numGuess >= maxGuess)
+          {
+              Console.WriteLine("You have lost the game.\n Your grandparents are dissapointed in you.")
+          }
+      }
+        
+        
     }
 }
