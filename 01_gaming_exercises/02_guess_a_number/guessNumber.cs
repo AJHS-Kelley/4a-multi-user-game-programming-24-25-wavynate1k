bc7e57fb61baf9b713ebe9d9955c6ac507ff13d7
wavynate1k <nathanspaul12345@icloud.com>
2024-09-06 13:57:18 -0400

    guessnumber

---
+++ 01_gaming_exercises/02_guess_a_number/guessNumber.cs
@@ -0,0 +1,16 @@
+using System;
+class GuessNumber {
+    static void Main() {
+      int numGuess = 0;
+      int maxGuess = 7;
+
+      // Generate the secret number here.
+      // while loop
+        // Allow the user to guess the number.
+        // Compare the guess to the secret number.
+        // If match, print win, if > or < give a hint.
+        // +1 to numGuess
+        // Check if numGuess > maxGuesse
+        // If true, print a "lose game" message, if false guess again.
+    }
+}
