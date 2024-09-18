7f8d33bee5b36d5fb1e5d95272ddd57df29ea1ac
wavynate1k <nathanspaul12345@icloud.com>
2024-09-18 14:55:32 -0400

    update

--- 01_gaming_exercises/04_rock_paper_scissors/rock_paper_scissors.cs
+++ 01_gaming_exercises/04_rock_paper_scissors/rock_paper_scissors.cs
@@ -1,36 +1,60 @@
 using System;
-class Template{
+class Template {
     static void Main() {
-
         Console.WriteLine("Enter your username.");
         string playerName = Console.ReadLine();
 
-        int playerScore;
-        int cpuScore;
-        
-        string cpuChoice;
-        Console.WriteLine(cpuChoice);
+        int playerScore = 0;
+        int cpuScore = 0;
 
-        Console.WriteLine("Is your pick rock, paper, or scisscors?");
-        string playerChoice = Console.ReadLine();
+        Random r = new Random();
 
-        if (playerChoice = "rock" && cpuChoice = "scisscors")
-        {
-            Console.WriteLine("You beat the CPU! It picked scisscors.");
-            playerScore++;
+        // Loop until either the player or the CPU reaches a score of 5
+        while (playerScore < 5 && cpuScore < 5) {
+            Console.WriteLine("Is your pick rock, paper, or scissors?");
+            string playerChoice = Console.ReadLine();
+
+            int cpuChoice = r.Next(1, 4);  // 1 = rock, 2 = paper, 3 = scissors
+            Console.WriteLine(cpuChoice);  // To see what the CPU picked
+
+            if (playerChoice == "rock" && cpuChoice == 3) {
+                Console.WriteLine("You beat the CPU! It picked scissors.");
+                playerScore++;
+            }
+            else if (playerChoice == "paper" && cpuChoice == 1) {
+                Console.WriteLine("You beat the CPU! It picked rock.");
+                playerScore++;
+            }
+            else if (playerChoice == "scissors" && cpuChoice == 2) {
+                Console.WriteLine("You beat the CPU! It picked paper!");
+                playerScore++;
+            }
+            else if (playerChoice == "scissors" && cpuChoice == 1) {
+                Console.WriteLine("You lost the round! CPU chose rock!");
+                cpuScore++;
+            }
+            else if (playerChoice == "rock" && cpuChoice == 2) {
+                Console.WriteLine("You lost the round! CPU chose paper!");
+                cpuScore++;
+            }
+            else if (playerChoice == "paper" && cpuChoice == 3) {
+                Console.WriteLine("You lost the round! CPU chose scissors!");
+                cpuScore++;
+            }
+            else {
+                Console.WriteLine("You picked the same choice as CPU!");
+            }
+
+            // Show the current scores
+            Console.WriteLine($"Player Score: {playerScore} - CPU Score: {cpuScore}");
         }
-        if (playerChoice = "paper" && cpuChoice = "rock") 
-        {
-            Console.WriteLine("You beat the CPU! It picked rock.");
-            playerScore++;
+
+        // End the game when someone reaches 5 points
+        if (cpuScore == 5) {
+            Console.WriteLine("You lost to the CPU, you really suck.");
         }
-        if (playerChoice = "scisscors" && cpuChoice = "paper")
-        {
-            Console.WriteLine("You beat the CPU! It picked paper.");
-            playerScore++;
+        else if (playerScore == 5) {
+            Console.WriteLine("You beat the CPU, Good Job!");
         }
-
-
-
     }
 }
